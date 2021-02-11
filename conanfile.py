6d2659d640b1ab9e8682586d79d59cab3f81a8f6
Björn Stresing <bjoern.stresing@gmx.de>
2021-02-12 00:38:08 +0100

    Switch to Android cmdline tools

--- conanfile.py
+++ conanfile.py
@@ -10,24 +10,24 @@ import os
 
 class AndroidSDKConan(ConanFile):
     name = "android-sdk"
-    version = "26.1.1"
-    description = "Android SDK Tools is a component for the Android SDK. It includes the complete set of development and debugging tools for Android"
+    version = "latest"
+    description = "Android commandline tools to obtain the Android SDK. It includes the complete set of development and debugging tools for Android"
     url = "https://github.com/Tereius/conan-android-sdk"
-    homepage = "https://developer.android.com/studio/releases/sdk-tools"
+    homepage = "https://developer.android.com/studio#cmdline-tools"
     license = "Apache 2.0"
     short_paths = True
     no_copy_source = True
     build_requires = "java_installer/8.0.144@tereius/stable"
     options = {"bildToolsRevision": "ANY"}
     default_options = "bildToolsRevision=28.0.2"
-    settings = {"os": ["Android"], "os_build": ["Windows", "Linux", "Macos"], "arch_build": ["x86", "x86_64"]}
+    settings = {"os": ["Android"], "os_build": ["Windows", "Linux", "Macos"], "arch_build": ["x86_64"]}
 
     min_api_level = 7
-    max_api_level = 28
+    max_api_level = 30
 
     @property
     def sdkmanager_bin(self):
-        return os.path.join(self.source_folder, "tools", "bin", "sdkmanager")
+        return os.path.join(self.source_folder, "cmdline-tools", "bin", "sdkmanager")
 
     def configure(self):
         if int(str(self.settings.os.api_level)) < self.min_api_level or int(str(self.settings.os.api_level)) > self.max_api_level:
@@ -35,33 +35,33 @@ class AndroidSDKConan(ConanFile):
 
     def source(self):
         if self.settings.os_build == 'Windows':
-            source_url = "https://dl.google.com/android/repository/sdk-tools-windows-4333796.zip"
-            tools.get(source_url, sha256="7e81d69c303e47a4f0e748a6352d85cd0c8fd90a5a95ae4e076b5e5f960d3c7a")
+            source_url = "https://dl.google.com/android/repository/commandlinetools-win-6858069_latest.zip"
+            tools.get(source_url)
         elif self.settings.os_build == 'Linux':
-            source_url = "https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip"
-            tools.get(source_url, sha256="92ffee5a1d98d856634e8b71132e8a95d96c83a63fde1099be3d86df3106def9", keep_permissions=True)
+            source_url = "https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip"
+            tools.get(source_url, keep_permissions=True)
         elif self.settings.os_build == 'Macos':
-            source_url = "https://dl.google.com/android/repository/sdk-tools-darwin-4333796.zip"
-            tools.get(source_url, sha256="ecb29358bc0f13d7c2fa0f9290135a5b608e38434aad9bf7067d0252c160853e", keep_permissions=True)
+            source_url = "https://dl.google.com/android/repository/commandlinetools-mac-6858069_latest.zip"
+            tools.get(source_url, keep_permissions=True)
         else:
             raise ConanException("Unsupported build os: " + self.settings.os_build)
 
     def build(self):
-        p = Popen([self.sdkmanager_bin, '--licenses'], universal_newlines=True, shell=True if self.settings.os_build == "Windows" else False, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        p = Popen([self.sdkmanager_bin, '--sdk_root=%s' % self.build_folder, '--licenses'], universal_newlines=True, shell=True if self.settings.os_build == "Windows" else False, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
         p.communicate(input='y\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\n')
-        self.run('%s --install "platforms;android-%s"' % (self.sdkmanager_bin, str(self.settings.os.api_level)))
-        self.run('%s --install "build-tools;%s"' % (self.sdkmanager_bin, str(self.options.bildToolsRevision)))
-        self.run('%s --install "platform-tools"' % (self.sdkmanager_bin))
+        self.run('%s --sdk_root=%s --install "platforms;android-%s"' % (self.sdkmanager_bin, self.build_folder, str(self.settings.os.api_level)))
+        self.run('%s --sdk_root=%s --install "build-tools;%s"' % (self.sdkmanager_bin, self.build_folder, str(self.options.bildToolsRevision)))
+        self.run('%s --sdk_root=%s --install "platform-tools"' % (self.sdkmanager_bin, self.build_folder))
 
     sdk_copied = False
 
     def package(self):
         # Called twice because of 'no_copy_source'. First from source-, then from build-dir
         if not self.sdk_copied:
-            copytree(os.path.join(self.source_folder, "build-tools"), os.path.join(self.package_folder, "build-tools"))
-            copytree(os.path.join(self.source_folder, "licenses"), os.path.join(self.package_folder, "licenses"))
-            copytree(os.path.join(self.source_folder, "platforms"), os.path.join(self.package_folder, "platforms"))
-            copytree(os.path.join(self.source_folder, "tools"), os.path.join(self.package_folder, "tools"))
+            copytree(os.path.join(self.build_folder, "build-tools"), os.path.join(self.package_folder, "build-tools"))
+            copytree(os.path.join(self.build_folder, "licenses"), os.path.join(self.package_folder, "licenses"))
+            copytree(os.path.join(self.build_folder, "platforms"), os.path.join(self.package_folder, "platforms"))
+            copytree(os.path.join(self.build_folder, "tools"), os.path.join(self.package_folder, "tools"))
             self.sdk_copied = True
 
     def package_id(self):
